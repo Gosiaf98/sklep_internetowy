f3ff77c2467dfc256145ee24a492f4fa4dfc9cd9
inf132221 <malgorzata.frackowiak@student.put.poznan.pl>
2020-05-28 17:14:29 +0200

    database cofiguration

---
+++ Backend/src/Entity/Product.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $pages_num;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $soft_cover;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $img_code;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getPagesNum(): ?int
+    {
+        return $this->pages_num;
+    }
+
+    public function setPagesNum(?int $pages_num): self
+    {
+        $this->pages_num = $pages_num;
+
+        return $this;
+    }
+
+    public function getSoftCover(): ?bool
+    {
+        return $this->soft_cover;
+    }
+
+    public function setSoftCover(?bool $soft_cover): self
+    {
+        $this->soft_cover = $soft_cover;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getImgCode(): ?string
+    {
+        return $this->img_code;
+    }
+
+    public function setImgCode(string $img_code): self
+    {
+        $this->img_code = $img_code;
+
+        return $this;
+    }
+}
